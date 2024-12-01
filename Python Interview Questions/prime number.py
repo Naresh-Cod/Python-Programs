fd9104f7131ad125a4304ad0f77b6c79fd587e7b
Code icon <110252548+Naresh-Cod@users.noreply.github.com>
2024-12-01 20:12:17 +0500

    Add files via upload

---
+++ Python Interview Questions/prime number.py
@@ -0,0 +1,31 @@
+def prime(val):
+    for i in range(2, val):
+        if val % i == 0:
+            break
+
+    else:
+        print("prime", val)
+
+for usr in range(2, 21):
+    prime(usr)
+
+
+prime_val = set()
+
+# ----------------------------------------
+
+def prime(val):
+    for i in range(2, val):
+        if val % i == 0:
+            break
+    else:
+        prime_val.add(val)
+
+N = 5
+
+num = 2
+while len(prime_val) < N:
+    prime(num)
+    num += 1
+
+print("Pehle", N, "prime numbers:", prime_val)
