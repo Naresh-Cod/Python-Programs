f8c7357a62527eec00e7bf45a5ab1e2ae131502c
Code icon <110252548+Naresh-Cod@users.noreply.github.com>
2024-12-03 22:03:25 +0500

    Add files via upload

---
+++ Python Interview Questions/Triangle find.py
@@ -0,0 +1,5 @@
+x = int(input("Enter a Angle 1: "))
+y = int(input("Enter a Angle 2: "))
+z = int(input("Enter a Angle 3: "))
+triangle = x+y+z == 180
+print("The given angles can form a triangle.")
