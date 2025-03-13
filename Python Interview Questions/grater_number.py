d81fb1f1b573f3089ac5a47573ce65414b0535cf
Code icon <110252548+Naresh-Cod@users.noreply.github.com>
2025-03-13 08:51:15 +0500

    Create grater_number.py

---
+++ Python Interview Questions/grater_number.py
@@ -0,0 +1,12 @@
+a = 33
+b = 11
+c = 33
+if a > b:
+    if a > c:
+        print('grater a = ', a)
+    else:
+        print('grater c = ', c)
+elif b > c:
+    print('grater b = ', b)
+else:
+    print('grater c = ', c)
