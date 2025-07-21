9e6986d9f95880047dce4047689a77b6175a6ab9
Code icon <110252548+Naresh-Cod@users.noreply.github.com>
2025-07-21 23:51:54 +0530

    Create data_type.py

---
+++ Python Interview Questions/data_type.py
@@ -0,0 +1,49 @@
+age = 25 # this is a number (int)
+name = 'rahul' # this is text (string)
+pi = 3.14 # this is a decimal (float)
+
+# int - number
+a = 10
+b = -5
+
+# float - decimal numbers
+p = 4.5
+
+# complex - used in advanced math
+z = 2 + 3j
+v = 8j
+
+# str = string(text)
+na = 'ravi'
+msg = 'hello, world!'
+
+# Boolean type
+# bool - True or False
+pia = True
+hi = False
+
+# (1, 2, 3, 4, 5)
+
+# list - ordered, changeable, allows duplicate
+lis = ['apple', 1, 2, 44, 54, 5.5, 1]
+
+# tuple - ordered, unchangeable, allows duplicate
+col = ('red', 5, 9.9)
+
+# range - a sequence of numbers
+number = range(5) # 0, 1, 2, 3, 4
+
+# mapping type - dict
+# key-value pair
+student = {'name':'ravi','age':20}
+print(student)
+
+# set = unordered,no duplicates
+se = {1, 2, 3, 4, 1, 44}
+
+
+# converting one type to anothe
+gh = '5'
+change = complex(gh)
+print(change)
+print(type(change))
