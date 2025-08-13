42b9cd434853eb1cf792244107936c78af7c6e5c
Code icon <110252548+Naresh-Cod@users.noreply.github.com>
2025-08-13 22:00:51 +0530

    Create Reverse a String.py

---
+++ Python Interview Questions/Reverse a String.py
@@ -0,0 +1,6 @@
+# Question: How do you reverse a string in Python without using built-in reverse functions?
+def reverse_string(s):
+    return s[::-1]
+
+# Example
+print(reverse_string("Hello"))  # Output: olleH
